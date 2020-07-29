a257859bf693a29228b00fdf6df9ba6aaad63939
tahir.appiskey <tahir.saleem@appiskey.com>
2020-07-29 17:01:25 +0500

    first

---
+++ src/main/java/com/baeldung/controller/AvroController.java
@@ -0,0 +1,24 @@
+package com.baeldung.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.producer.AvroProducer;
+
+@RestController
+public class AvroController {
+
+    @Autowired
+    private AvroProducer avroProducer;
+
+    @GetMapping("/test")
+//    public String producerAvroMessage(@PathVariable int id, @PathVariable String firstName, @PathVariable String lastName) {
+        public String producerAvroMessage() {
+        avroProducer.produceEmployeeDetails(1, "Tahir", "lastName");
+        return "Sent employee details to consumer";
+    }
+
+}
